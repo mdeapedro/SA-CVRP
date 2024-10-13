8218d2d3f70f04ca72163bf86c3ce7f789e7bd4c
Pedro <mdeapedro@gmail.com>
2024-10-12 23:43:36 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "sa.h"
 
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("Hello, World!\n");
+    if (argc < 2) return 1;
+
+    Instance *instance = load_instance(argv[1]);
+    if (!instance) return 1;
+
+    InputParameters ip = { instance, 10, time(NULL) };
+    OutputParameters op = { stdout };
+    SA_Parameters sap = { 0.99999, 1.0 };
+
+    Solution *solution = sa_solve(ip, op, sap);
+
+    free_solution(solution);
+    free_instance(instance);
 }
